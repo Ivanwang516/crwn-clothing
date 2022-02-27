a0f93fb810c86dd0dfc5ef95b158eb9eade08c1e
Ivanwang516 <Ivanwang516@gmail.com>
2022-02-27 15:31:56 +1100

    updated app with a shop page, collection preview component, and collection item component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import './App.css';
 
 import HomePage from './pages/homepage/hompage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-      
+    <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/shop' element={<ShopPage />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
